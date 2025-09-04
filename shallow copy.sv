253d607c73a1dbaaed313b0563d90651339d9666
Nikhila Batchu <nikhilabatchu9363@gmail.com>
2025-09-04 12:23:09 +0530

    Create shallow copy.sv

---
+++ shallow copy.sv
@@ -0,0 +1,27 @@
+// Code your testbench here
+// or browse Examples
+class vlsi;
+  int a;
+  int b;
+endclass
+
+module tb;
+  vlsi v1,v2;
+  initial begin 
+  	v1=new();
+  	v2=new();
+  	v2.a=10;
+  	v1=new v2; //v1=v2.a and v2.b or v1.a and v1.b= v2.a and v2.b
+  //in shallow copy seperate memory locations are allocated for destination and source handles.
+ 	 $display("v1=%0d",v1.a);
+ 	 $display("v1=%0p",v1);
+ 	 v2.a=20;
+ 	 $display("v1=%0p",v1);
+  end
+endmodule
+
+//output:
+# KERNEL: v1=10
+# KERNEL: v1=10 0
+# KERNEL: v1=10 0
+  
